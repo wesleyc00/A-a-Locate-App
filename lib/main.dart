9b1e4dbc090b1c4665edfdfb35ad17e4a4c0e70e
Wesley Corrêa <wesleyeng00@gmail.com>
2024-06-07 14:24:57 -0300

    Validacao de Email e Senha

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/cadastro.dart';
 import 'screens/home.dart';
+
 void main() {
   runApp(const AcaiLocate());
 }
@@ -21,15 +22,35 @@ class AcaiLocate extends StatelessWidget {
       routes: {
         '/home': (context) => const HomeScreen(),
         '/login': (context) => const AutenticaAcai(),
-        '/cadastro': (context) =>
-            const CadastroUsuario(), // Rota para a tela de cadastro
+        '/cadastro': (context) => const CadastroUsuario(),
       },
     );
   }
 }
 
-class AutenticaAcai extends StatelessWidget {
+class AutenticaAcai extends StatefulWidget {
   const AutenticaAcai({super.key});
+
+  @override
+  State<AutenticaAcai> createState() => _AutenticaAcaiState();
+}
+
+class _AutenticaAcaiState extends State<AutenticaAcai> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  String? validateEmail(String? value) {
+    const pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
+    final regExp = RegExp(pattern);
+
+    if (value == null || value.isEmpty) {
+      return 'Por favor, insira seu e-mail';
+    } else if (!regExp.hasMatch(value)) {
+      return 'Por favor, insira um e-mail válido';
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,94 +58,108 @@ class AutenticaAcai extends StatelessWidget {
       body: Center(
         child: SingleChildScrollView(
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 230,
-                  child: Image.asset('assets/logo.png'),
-                ),
-                const SizedBox(height: 20), // Espaço entre os campos
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 230,
+                    child: Image.asset('assets/logo.png'),
+                  ),
+                  const SizedBox(height: 20), // Espaço entre os campos
 
-                const Row(
-                  children: [
-                    SizedBox(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 55.0),
-                        child: Text(
-                          'Login',
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            color: Color(0xFF509C00),
-                            fontWeight: FontWeight.bold,
+                  const Row(
+                    children: [
+                      SizedBox(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 55.0),
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Color(0xFF509C00),
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10), // Espaço entre os campos
-                const SizedBox(
-                  width: 300, // Defina a largura desejada
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'E-mail',
-                    ),
+                    ],
                   ),
-                ),
-                const SizedBox(height: 20), // Espaço entre os campos
-                const SizedBox(
-                  width: 300, // Defina a largura desejada
-                  child: TextField(
-                    obscureText: true, // Oculta o texto para o campo de senha
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Senha',
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 60), // Espaço entre os campos
-                SizedBox(
-                  width: 200,
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/home');
-                    },
-                    child: const Text('Entrar'),
-                  ),
-                ),
-                const SizedBox(height: 40), // Espaço entre os campos
-                Row(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(left: 97, right: 5),
-                      child: Text(
-                        'Não tem uma conta?',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14.0,
-                        ),
+                  const SizedBox(height: 10), // Espaço entre os campos
+                  SizedBox(
+                    width: 300, // Defina a largura desejada
+                    child: TextFormField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'E-mail',
                       ),
+                      validator: validateEmail,
                     ),
-                    InkWell(
-                      onTap: () {
-                        Navigator.pushNamed(context, '/cadastro');
+                  ),
+                  const SizedBox(height: 20), // Espaço entre os campos
+                  SizedBox(
+                    width: 300, // Defina a largura desejada
+                    child: TextFormField(
+                      controller: _passwordController,
+                      obscureText: true, // Oculta o texto para o campo de senha
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Senha',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, insira sua senha';
+                        }
+                        return null;
                       },
-                      child: const Text(
-                        'Cadastre-se',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14.0,
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline,
+                    ),
+                  ),
+                  const SizedBox(height: 60), // Espaço entre os campos
+                  SizedBox(
+                    width: 200,
+                    height: 40,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState?.validate() ?? false) {
+                          Navigator.pushNamed(context, '/home');
+                        }
+                      },
+                      child: const Text('Entrar'),
+                    ),
+                  ),
+                  const SizedBox(height: 40), // Espaço entre os campos
+                  Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(left: 97, right: 5),
+                        child: Text(
+                          'Não tem uma conta?',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14.0,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      InkWell(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/cadastro');
+                        },
+                        child: const Text(
+                          'Cadastre-se',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14.0,
+                            color: Colors.blue,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/screens/cadastro.dart
+++ lib/screens/cadastro.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 
-class CadastroUsuario extends StatelessWidget {
+class CadastroUsuario extends StatefulWidget {
   const CadastroUsuario({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _CadastroUsuarioState createState() => _CadastroUsuarioState();
+}
+
+class _CadastroUsuarioState extends State<CadastroUsuario> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+
+  bool arePasswordsMatching(String password, String confirmPassword) {
+    return password == confirmPassword;
+  }
+
+  String? validateEmail(String? value) {
+    const pattern =
+        r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
+    final regExp = RegExp(pattern);
+
+    if (value == null || value.isEmpty) {
+      return 'Por favor, insira seu e-mail';
+    } else if (!regExp.hasMatch(value)) {
+      return 'Por favor, insira um e-mail válido';
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,106 +37,132 @@ class CadastroUsuario extends StatelessWidget {
       body: Center(
         child: SingleChildScrollView(
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 230,
-                  child: Image.asset('assets/logo.png'),
-                ),
-                const SizedBox(height: 20), // Espaço entre os campos
-                // ignore: prefer_const_constructors
-                Row(
-                  children: const [
-                    SizedBox(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 55.0),
-                        child: Text(
-                          'Cadastre-se',
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            color: Color(0xFF509C00),
-                            fontWeight: FontWeight.bold,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 230,
+                    child: Image.asset('assets/logo.png'),
+                  ),
+                  const SizedBox(height: 20), // Espaço entre os campos
+                  const Row(
+                    children: [
+                      SizedBox(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 55.0),
+                          child: Text(
+                            'Cadastre-se',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Color(0xFF509C00),
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10), // Espaço entre os campos
-                const SizedBox(
-                  width: 300, // Defina a largura desejada
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'E-mail',
-                    ),
+                    ],
                   ),
-                ),
-                const SizedBox(height: 20), // Espaço entre os campos
-                const SizedBox(
-                  width: 300, // Defina a largura desejada
-                  child: TextField(
-                    obscureText: true, // Oculta o texto para o campo de senha
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Senha',
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20), // Espaço entre os campos
-                const SizedBox(
-                  width: 300, // Defina a largura desejada
-                  child: TextField(
-                    obscureText: true, // Oculta o texto para o campo de senha
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Repita a Senha',
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 60), // Espaço entre os campos
-                SizedBox(
-                  width: 200,
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Ação ao clicar no botão
-                      print('Botão pressionado!');
-                    },
-                    child: const Text('Cadastrar'),
-                  ),
-                ),
-                const SizedBox(height: 40), // Espaço entre os campos
-                Row(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(left: 150, right: 5),
-                      child: Text(
-                        'Faça login',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14.0,
-                        ),
+                  const SizedBox(height: 10), // Espaço entre os campos
+                  SizedBox(
+                    width: 300, // Defina a largura desejada
+                    child: TextFormField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'E-mail',
                       ),
+                      validator: validateEmail,
                     ),
-                    InkWell(
-                      onTap: () {
-                        Navigator.pushNamed(context, '/login');
+                  ),
+                  const SizedBox(height: 20), // Espaço entre os campos
+                  SizedBox(
+                    width: 300, // Defina a largura desejada
+                    child: TextFormField(
+                      controller: _passwordController,
+                      obscureText: true, // Oculta o texto para o campo de senha
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Senha',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, insira sua senha';
+                        }
+                        return null;
                       },
-                      child: const Text(
-                        'Login',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14.0,
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline,
+                    ),
+                  ),
+                  const SizedBox(height: 20), // Espaço entre os campos
+                  SizedBox(
+                    width: 300, // Defina a largura desejada
+                    child: TextFormField(
+                      controller: _confirmPasswordController,
+                      obscureText: true, // Oculta o texto para o campo de senha
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Repita a Senha',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, repita sua senha';
+                        }
+                        if (!arePasswordsMatching(_passwordController.text, value)) {
+                          return 'As senhas não coincidem';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 60), // Espaço entre os campos
+                  SizedBox(
+                    width: 200,
+                    height: 40,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState?.validate() ?? false) {
+                          // As senhas coincidem e a validação foi bem-sucedida
+                          Navigator.pushNamed(context, '/home');
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Cadastro realizado com sucesso!')),
+                          );
+                        }
+                      },
+                      child: const Text('Cadastrar'),
+                    ),
+                  ),
+                  const SizedBox(height: 40), // Espaço entre os campos
+                  Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(left: 150, right: 5),
+                        child: Text(
+                          'Faça login',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14.0,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      InkWell(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/login');
+                        },
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14.0,
+                            color: Colors.blue,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
